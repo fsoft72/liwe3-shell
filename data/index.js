b4e208b3b59cca92ba940320740330cadf9998a5
fsoft <fabio.rotondo@gmail.com>
2026-04-26 10:58:56 +0200

    v 0.7.0

--- data/index.js
+++
@@ -1,5 +0,0 @@
-const svelteFiles = require( './svelte_files.js' );
-
-module.exports = {
-	svelteFiles
-};
--- index.js
+++ index.js
@@ -1,115 +1,45 @@
 #!/usr/bin/env node
 
-const VERSION = '0.6.1';
+const VERSION = "0.7.0";
 
-const fs = require( 'fs' );
-const yargs = require( 'yargs/yargs' );
-const { hideBin } = require( 'yargs/helpers' );
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 
-const {
-	checkIsSvelte,
-	resolvePackageManager,
-	checkGit,
-} = require( './data/utils' );
+const { cfCreate, cfInit, cfUpdate } = require("./data/cf_ops");
 
-const {
-	svelteInit,
-	svelteCreateProject,
-	svelteAddSubmodule,
-} = require( './data/svelte_ops' );
+const argv = yargs(hideBin(process.argv))
+	.usage("Usage: $0 <command> [options]")
 
-const {
-	nodeCreateProject,
-	nodeAddSubmodule,
-} = require( './data/nodejs_ops' );
+	.command(
+		"create [folder]",
+		"Create new LiWE3 Cloudflare project",
+		(yargs) => {
+			return yargs.positional("folder", {
+				describe:
+					"Project folder name (uses current dir if omitted, must be empty)",
+				type: "string",
+			});
+		},
+	)
+	.command("init", "Initialize working repo (install deps, run setup scripts)")
+	.command(
+		["update", "upgrade"],
+		"Merge latest liwe3-cloudflare changes into current repo",
+	)
+	.version(VERSION)
+	.help().argv;
 
-
-const nodeInit = ( pm, nodeServerPort ) => {
-	if ( !fs.existsSync( 'package.json' ) ) {
-		console.log( "\n\nERROR: this does not seem to be a Node project.\n" );
-		return;
-	}
-	console.log( 'Initializing LiWE3 Node project...' );
-
-	if ( !checkGit() ) return;
-};
-
-const argv = yargs( hideBin( process.argv ) )
-	.usage( 'Usage: $0 <command> [options]' )
-
-	.command( 'init', 'Initialize project' )
-	.command( 'create <type> <name>', 'Create new project', ( yargs ) => {
-		return yargs
-			.positional( 'type', {
-				describe: 'Project type (nodejs or svelte)',
-				type: 'string',
-				choices: [ 'nodejs', 'svelte' ]
-			} )
-			.positional( 'name', {
-				describe: 'Project name',
-				type: 'string'
-			} );
-	} )
-	.command( 'addsubmodule [submodules..]', 'Add submodules to project', ( yargs ) => {
-		return yargs.positional( 'submodules', {
-			describe: 'List of submodules to add',
-			type: 'string',
-			array: true
-		} );
-	} )
-	.option( 'node-server-port', {
-		alias: 'p',
-		type: 'number',
-		description: 'Set Node server port',
-		default: 12000
-	} )
-	.option( 'pm', {
-		type: 'string',
-		description: 'Set package manager',
-		choices: [ 'npm', 'yarn', 'pnpm' ],
-		default: 'pnpm'
-	} )
-	.version( VERSION )
-	.help()
-	.argv;
-
-// get the package manager (default to npm)
-argv.pm = argv.pm || resolvePackageManager();
-
-let isSvelte = checkIsSvelte();
-
-
-switch ( argv._[ 0 ] ) {
-	case 'init':
-		console.log( 'Initializing project...' );
-		if ( argv.pm ) console.log( `Using package manager: ${ argv.pm }` );
-
-		if ( isSvelte ) {
-			console.log( 'Svelte project detected' );
-			svelteInit( argv.pm, argv.nodeServerPort );
-		} else {
-			console.log( 'Node project detected' );
-			nodeInit( argv.pm, argv.nodeServerPort );
-		}
-
+switch (argv._[0]) {
+	case "create":
+		cfCreate(argv.folder);
 		break;
-	case 'addsubmodule':
-		console.log( 'Adding submodules:', argv.submodules );
-		if ( isSvelte ) {
-			console.log( 'Svelte project detected' );
-			svelteAddSubmodule( argv.submodules );
-		} else {
-			console.log( 'Node project detected' );
-			nodeAddSubmodule( argv.submodules );
-		}
+	case "init":
+		cfInit();
 		break;
-	case 'create':
-		console.log( `Creating ${ argv.type } project: ${ argv.name }` );
-		if ( argv.type == 'nodejs' )
-			nodeCreateProject( argv.name, argv.pm, argv.nodeServerPort );
-		else if ( argv.type == 'svelte' )
-			svelteCreateProject( argv.name, argv.pm, argv.nodeServerPort );
+	case "update":
+	case "upgrade":
+		cfUpdate();
 		break;
 	default:
-		console.log( `Mode: ${ argv.node ? 'Node' : 'Svelte' }` );
+		yargs().showHelp();
 }
